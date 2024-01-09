bf6f8bce24a6860b7f15a775b2cf77faf6965b4e
Bonge <nkaranjah@gmail.com>
2024-01-09 12:05:42 +0300

    Add meta description and favicon

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,9 +1,18 @@
 <script setup>
 import Navbar from "@/Components/Navbar.vue";
 import { Modal } from "momentum-modal";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
+	<Head>
+		<meta
+			content="Discover TechLease - Your Premier Online Rental Destination for Technology Devices. TechLease is a leading online technology rental service offering flexibility, affordability, and accessibility to the latest gadgets. Our extensive inventory includes laptops, smartphones, VR headsets, digital cameras, and more! Ideal for both personal and business use, our eco-conscious solution allows customers to enjoy state-of-the-art technology without lengthy commitments or exorbitant prices. Simplify decision making with our intuitive website, dedicated customer assistance, and streamlined management tools for all your rental needs. Upgrade smarter today with TechLease – where innovation meets responsibility."
+			head-key="description"
+			name="description"
+		/>
+		<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	</Head>
 	<Navbar />
 
 	<main>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description"
+		  content="Discover TechLease - Your One-Stop Online Rental Destination for State-of-the-art Technology Devices. TechLease offers a vast selection of premium technology devices available for flexible rental-periods, catering to individual and business needs alike. Browse our extensive inventory including laptops, smartphones, VR headsets, digital cameras, and more! Our easy-to-use website ensures a smooth experience when choosing and managing your tech rentals. Enjoy cost savings, reduced e-waste, and expert customer support every step of the way at TechLease – The Smarter Way To Access Innovative Technologies!">
 
 	<title inertia>{{ config('app.name', 'Laravel') }}</title>
 
